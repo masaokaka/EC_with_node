2394cfe71fcd1226205c32fef192c0e7a25587ba
masaokaka <osushiiisensu@gmail.com>
2021-07-20 10:23:11 +0900

    api-only

--- front/src/components/atoms/test/LoadingPage.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { LoadingPage } from "../";
-
-describe("LoadingPage", () => {
-  test("正しく表示されているかテスト", () => {
-    render(<LoadingPage />);
-    expect(screen.getByRole("img")).toBeInTheDocument();
-  });
-});
--- front/src/static/admin.ts
+++
@@ -1,1 +0,0 @@
-export const ADMIN_ID = "gCy8PvKfIZcycRZSeQpNpbaL7Tk2";
